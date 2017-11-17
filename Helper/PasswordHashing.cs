92aa88506b70026ebc9b82381c6e0285af98ae76
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2017-11-17 23:34:35 +0700

    formating

--- Helper/PasswordHashing.cs
+++ Helper/PasswordHashing.cs
@@ -6,29 +6,29 @@ using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace diary.Helper
 {
-    public class PasswordHashing : IPasswordHasher<User>
-    {
-        public PasswordHashing()
-        {
+      public class PasswordHashing : IPasswordHasher<User>
+      {
+            public PasswordHashing()
+            {
 
-        }
+            }
 
-        public string HashPassword(User user, string password)
-        {
-            // this user is a registering user
+            public string HashPassword(User user, string password)
+            {
+                  // this user is a registering user
 
-            var data = System.Text.Encoding.ASCII.GetBytes(password);
-            data = System.Security.Cryptography.SHA1.Create().ComputeHash(data);
-            password = Convert.ToBase64String(data);
+                  var data = System.Text.Encoding.ASCII.GetBytes(password);
+                  data = System.Security.Cryptography.SHA1.Create().ComputeHash(data);
+                  password = Convert.ToBase64String(data);
 
-            return password;
-        }
-        public PasswordVerificationResult VerifyHashedPassword(User user, string userpwd, string pwd)
-        {
-            if (userpwd == HashPassword(user, pwd))
-                return PasswordVerificationResult.Success;
-            else
-                return PasswordVerificationResult.Failed;
-        }
-    }
+                  return password;
+            }
+            public PasswordVerificationResult VerifyHashedPassword(User user, string userpwd, string pwd)
+            {
+                  if (userpwd == HashPassword(user, pwd))
+                        return PasswordVerificationResult.Success;
+                  else
+                        return PasswordVerificationResult.Failed;
+            }
+      }
 }
