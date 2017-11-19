cc7d958d8735d02abf9b4bac48830711b3364179
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2017-11-19 21:20:49 +0700

    calendar event change

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using diary.Data;
+using diary.Models;
 
 namespace diary.Controllers
 {
       public class ScheduleController : Controller
       {
             diaryContext _context;
-            public ScheduleController(diaryContext context)
+            SignInManager<User> _signinmanager;
+
+            public ScheduleController(
+                  diaryContext context,
+                  SignInManager<User> signinmanager)
             {
                   _context = context;
+                  _signinmanager = signinmanager;
             }
 
-            public IActionResult Index()
+            public IActionResult Index(int day, int month, int year)
             {
+                  if (!_signinmanager.IsSignedIn(User))
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+
                   return View();
             }
       }
