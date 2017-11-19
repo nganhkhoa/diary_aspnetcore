7fe1df170af439a297f00054f93ef9cd391648cb
Khoa Nguyen Anh <ng.akhoa@yahoo.com.vn>
2017-11-19 13:23:19 +0700

    schedule routing after login

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using diary.Data;
 
 namespace diary.Controllers
 {
-    public class ScheduleController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+      public class ScheduleController : Controller
+      {
+            diaryContext _context;
+            public ScheduleController(diaryContext context)
+            {
+                  _context = context;
+            }
+
+            public IActionResult Index()
+            {
+                  return View();
+            }
+      }
 }
