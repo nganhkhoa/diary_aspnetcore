d92002bd5b5e51d9242435d5169a01f236d534fb
khoitran <zzkhoizz123@gmail.com>
2017-11-21 20:38:38 +0700

    updateSchedule

--- Models/User.cs
+++ Models/User.cs
@@ -4,16 +4,21 @@ using Microsoft.AspNetCore.Identity;
 
 namespace diary.Models
 {
-      public class User : IdentityUser
-      {
-            // Id, UserName, Email are in IdentityUser
+    public class User : IdentityUser
+    {
 
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public DateTime Brithday { get; set; }
 
-            // navigation properties
-            public ICollection<Event> Events { get; set; }
-            public ICollection<Entry> Entries { get; set; }
-      }
+        private UserContext context;
+
+        // Id, UserName, Email are in IdentityUser
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Brithday { get; set; }
+
+        // navigation properties
+        public ICollection<Event> Events { get; set; }
+        public ICollection<Entry> Entries { get; set; }
+    }
 }
